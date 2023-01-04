aeff96ee8008dd891416d0f1ec990b212b3a149a
kudr1kk22 <kudr1kk22@gmail.com>
2023-01-04 20:20:53 +0300

    Initial Commit

---
+++ MPSpotify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MPSpotify
+//
+//  Created by Eugene Kudritsky on 4.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
